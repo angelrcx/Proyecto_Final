157d60eb87077f590898abb0cce50f4f4ed437bd
angelrcx <hielorc@gmail.com>
2024-05-27 19:24:33 -0600

    Final

--- djangoproject/myapp/models.py
+++ djangoproject/myapp/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Reservacion(models.Model):
+    HABITACION_CHOICES = [
+        ('individual', 'Individual'),
+        ('doble', 'Doble'),
+        ('suite', 'Suite'),
+        ('penhouse','Penhouse')
+    ]
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    #nombre_completo = models.CharField(max_length=50)
+    #direccion = models.CharField(max_length=50)
     fecha_entrada = models.DateField()
     fecha_salida = models.DateField()
-    tipo_habitacion = models.CharField(max_length=50)
+    tipo_habitacion = models.CharField(max_length=50, choices=HABITACION_CHOICES)
     numero_huespedes = models.PositiveIntegerField()
 
     def __str__(self):
